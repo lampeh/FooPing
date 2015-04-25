2ba050f37b30dbfc7108f7e335e8733885717161
Hauke Lampe <lampe@hauke-lampe.de>
2015-04-25 20:29:36 +0200

    updated from AndroidStudio branch

--- server/FooPingReceiver.py
+++ server/FooPingReceiver.py
@@ -3,6 +3,7 @@ import socket
 import json
 import os
 import hashlib
+import hmac
 from StringIO import StringIO
 from Crypto.Cipher import AES
 from gzip import GzipFile
@@ -11,8 +12,22 @@ from gzip import GzipFile
 UDP_IP = "0.0.0.0"
 UDP_PORT = 23042
 
-key = hashlib.sha256(b'm!ToSC]vb=:<b&XL.|Yq#L%YE{V+$Mc~y').digest()
+key = hashlib.sha256(b'm!ToSC]vb=:<b&XL.|Yq#LYE{V+$Mc~y').digest()
 block_size = 16
+
+mac_key = hashlib.sha256(b'sM[N9+l8~N7Ox_7^EI>s|vLkiVXo-[T').digest()
+mac_size = 20
+
+def compare_digest(x, y):
+	if not (isinstance(x, bytes) and isinstance(y, bytes)):
+		raise TypeError("both inputs should be instances of bytes")
+	if len(x) != len(y):
+		return False
+	result = 0
+	for a, b in zip(x, y):
+		result |= ord(a) ^ ord(b)
+	return result == 0
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -22,11 +37,19 @@ clientData = {}
 
 while True:
 	try:
-		data, addr = sock.recvfrom(1500)
+		raw, addr = sock.recvfrom(1500)
 		print "packet from: " + addr[0] + ":" + str(addr[1])
+
+		data = raw[:-mac_size]
+
+		mac = raw[-mac_size:]
+		mac2 = hmac.new(mac_key, data, hashlib.sha1).digest()
+		if (not compare_digest(mac, mac2)):
+			print "MAC mismatch!"
+			continue
+
 		msg = GzipFile(fileobj = StringIO(AES.new(key, AES.MODE_CFB, data[:block_size]).decrypt(data[block_size:]))).read()
 		print msg
-		print
 		packet = json.loads(msg)
 		pingData = {}
 		pingData.update(packet[0])
@@ -40,6 +63,6 @@ while True:
 		f.close()
 		os.rename('data.json.tmp', 'data.json');
 	except Exception:
-		print "*** exception in packet from: " + addr[0] + ":" + str(addr[1])
+		print "exception in packet from: " + addr[0] + ":" + str(addr[1])
 		print sys.exc_info()
 		pass
